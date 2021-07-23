43def257a7b8e5efa0ad1f0a5a5dfcb3c2be8d4a
Petar Netev <pesho.netev@gmail.com>
2021-07-23 22:35:58 +0300

    Fix transactions handler

--- transactions-handler/lambdas/transaction.py
+++ transactions-handler/lambdas/transaction.py
@@ -5,12 +5,15 @@ logger = logging.getLogger(__name__)
 
 
 def handler(event, context):
-    # logger.info('Event info: {event}'.format(event=json.dumps(event)))
-    #
-    # if 'transaction' not in event:
-    #     return {'statusCode': 400, 'message': 'No transaction info provided'}
-    #
+    logger.info('Event info: {event}'.format(event=event['body']))
+
+    event_body = json.loads(event['body'])
+    print(event_body)
+
+    if 'transactions' not in event_body:
+        return {'statusCode': 400, 'body': 'No transaction info provided'}
+
     return {
         'statusCode': 200,
-        'body': json.dumps(event['transaction']),
+        'body': json.dumps(event_body['transactions']),
     }
