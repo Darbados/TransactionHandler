93aa92ec63553f5f34062bf4b643700172c537bd
Petar Netev <pesho.netev@gmail.com>
2021-07-28 21:53:26 +0300

    Change the index handler to work with query parameters

--- transactions-handler/lambdas/index.py
+++ transactions-handler/lambdas/index.py
@@ -1,10 +1,8 @@
-import json
-
-
 def handler(event, context):
-    body = {
-        'message': 'Go Serverless v2.0! Your function executed successfully!',
-        'input': event,
-    }
-
-    return {'statusCode': 200, 'body': json.dumps(body)}
+    query_string_parameters = event.get('queryStringParameters', {})
+    if query_string_parameters:
+        message = 'Index url returned {composed_message}'.format(
+            composed_message=', '.join(query_string_parameters.values()))
+    else:
+        message = 'Index url has no GET parameters provided.'
+    return {'statusCode': 200, 'body': f' {message}'}
